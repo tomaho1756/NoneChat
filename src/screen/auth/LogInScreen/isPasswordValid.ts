617f7105bb1038b59f11a998a12b7b6bc0eef757
김의현(toma_to) <148212609+tomaho1756@users.noreply.github.com>
2024-04-27 02:43:16 +0900

    ✨Add : Add Details on LoginScreen And add some rule for SignIn

---
+++ src/screen/auth/LogInScreen/isPasswordValid.ts
@@ -0,0 +1,34 @@
+
+export const isPasswordValid = (password : string) : [boolean, number?] => {
+    
+    const passed: [boolean, boolean, boolean] = [false, false, false]
+    
+    const isUpperInclude = password.split("").map(i => i.charCodeAt(0)).map(i => i >= 65 && i <= 90)
+    
+    if (password.length >= 8) {
+        passed[0] = true
+    } else {
+        return [false, 0]
+    }
+    
+    
+    if (isUpperInclude.includes(true)) {
+        passed[1] = true
+    } else {
+        return [false, 1]
+    }
+    
+    
+    if(/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        passed[2] = true
+    } else {
+        return [false, 2]
+    }
+    
+    
+    if (passed[0] && passed[1] && passed[2]) {
+        return [true];
+    } else {
+        return [false];
+    }
+}
